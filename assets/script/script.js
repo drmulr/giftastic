d68b06fc686559197dafd974df388780bc68405d
Daniel Muller <DMuller@DM.cfl.rr.com>
2017-08-28 20:05:18 -0400

    Gif animations complete, add'l CSS Style

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-  // Initial array of movies
+  // Initial array of animals
   var animals = ["Lion", "Tiger", "Bear"];
 
   // Function for dumping the JSON content for each button into the div
@@ -12,65 +12,31 @@
       method: "GET"
       }).done(function(response) {
           $("#animalGifs").empty();
-          console.log(response.data[0].images.fixed_height.url);
-
           for( var i = 0; i < 11; i++){
               var gifUrl = response.data[i].images.original.url;
               var gifImage = $("<img>");
-              var gifUrlStill = response.data[i].images.fixed_height_still.url;
+              var gifUrlStill = response.data[i].images.original_still.url;
               var gifImageStill = $("<img>");
-
-              // gifImage.attr("src", gifUrl);
-              // gifImage.attr("data-still", gifImageStill);
-              // gifImage.attr("data-animate", gifUrl);
-              // gifImage.attr("data-state", "animate");
-              // gifImage.addClass("gif");
-              // gifImage.attr("alt", "gifImage");
-
               gifImageStill.attr("src", gifUrlStill);
               gifImageStill.attr("data-state", "still");
               gifImageStill.attr("data-still", gifUrlStill);
               gifImageStill.attr("data-animate", gifUrl);
               gifImageStill.addClass("gif");
-              gifImageStill.attr("alt", "Gif Image Still");
-
+              gifImageStill.attr("alt", "still gif");
               $("#animalGifs").append(gifImageStill);
-
-              console.log("Data: " + response);
-              console.log(response.data[0].images);
-              console.log("Still: " + gifUrlStill);
-
-              $(".gif").on("click", function() {
-                  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-                  var state = $(this).attr("data-state");
-                  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-                  // Then, set the image's data-state to animate
-                  // Else set src to the data-still value
-                  if (state === "still") {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                  } else {
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still");
-                  }
-                });
-
-//Rating append needs work
+              //Rating append needs work
               // console.log(gifRating);
               // var gifRating = response.data[i].rating;
               // $("#animalGifs").prepend("Rating: " + gifRating);
-
           }
-      renderButtons();
+    renderButtons();
     });
   }
-
-  // Function for displaying animal data
-  function renderButtons() {
-
+    // Function for displaying animal data
+    function renderButtons() {
     $("#animalButtons").empty();
-      // Looping through the array of movies
-      for (var i = 0; i < animals.length; i++) {
+        // Looping through the array of movies
+        for (var i = 0; i < animals.length; i++) {
         // Then dynamicaly generating buttons for each animal in the array
         var a = $("<button>");
         // Adding a class of movie to our button
@@ -82,8 +48,7 @@
         // Adding the button to the buttons-view div
         $("#animalButtons").append(a);
       }
-  }
-
+    }
   // This function handles events where one button is clicked
   $("#add-animal").on("click", function(event) {
     event.preventDefault();
@@ -95,10 +60,22 @@
     // Calling renderButtons which handles the processing of our movie array
     renderButtons();
   });
-
   // Function for displaying the animal info
   // Using $(document).on instead of $(".movie").on to add event listenersto dynamically generated elements
   $(document).on("click", ".animal", displayAnimalInfo);
-
-  // Calling the renderButtons function to display the intial buttons
+  $(document).on("click", ".gif", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+      } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+      }
+  });
+  // Render initial buttons
   renderButtons();
